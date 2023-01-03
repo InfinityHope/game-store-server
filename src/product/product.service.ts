9ef9e06319e84aca875904a6d8f0d3e147fee7c9
Nikita <bigbasspoll@gmail.com>
2023-01-03 22:35:54 +0300

    fixReturningProductsFields

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -17,6 +17,7 @@ export class ProductService {
 		return products.map((product) => ({
 			_id: product._id,
 			title: product.title,
+			availability: product.availability,
 			releaseDate: product.releaseDate,
 			img: product.img,
 			price: product.price,
